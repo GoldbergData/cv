ae4a63c3f17997092e45ec7892931a4080ce41b3
Joshua Goldberg <josh.goldberg1@outlook.com>
2025-04-28 17:16:56 -0700

    Some work

--- docs/render_cv.r
+++ docs/render_cv.r
@@ -18,4 +18,5 @@ rmarkdown::render("docs/cv.rmd",
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = "docs/goldberg_cv.pdf")
+                       output = "docs/goldberg_cv.pdf",
+                      )
